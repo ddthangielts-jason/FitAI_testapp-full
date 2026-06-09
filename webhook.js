d91daff16f3347d3c8a7feaa6cc8b3255b922afe
ddthangielts-jason <ddthang.ielts@gmail.com>
2026-06-09 22:44:32 +0700

    Delete webhook.js

--- webhook.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  // Handle Stripe webhooks here
-  return res.status(200).json({ received: true });
-}
